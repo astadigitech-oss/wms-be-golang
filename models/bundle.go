4493d16f602083af69fbcad48706fa038e76b9eb
windisiswandi <windisiswandi89@gmail.com>
2025-12-24 06:57:35 +0800

    update bundle struct

--- models/bundle.go
+++ models/bundle.go
@@ -5,12 +5,12 @@ import "time"
 type Bundle struct {
 	ID              uint64     `gorm:"primaryKey;autoIncrement" json:"id"`
 	UserID          *uint64    `json:"user_id"`
-	NameBundle      string    `gorm:"size:255" json:"name_bundle"`
-	TotalPrice      float64   `gorm:"type:decimal(18,2)" json:"total_price"`
-	TotalPriceCustom float64  `gorm:"type:decimal(18,2)" json:"total_price_custom"`
-	TotalProduct    int64     `json:"total_product"`
-	Status          string    `gorm:"size:50" json:"status"`
-	Barcode         string    `gorm:"size:255" json:"barcode"`
+	NameBundle      string    `gorm:"size:255;not null" json:"name_bundle"`
+	TotalPrice      float64   `gorm:"type:decimal(18,2);default:0" json:"total_price"`
+	TotalPriceCustom float64  `gorm:"type:decimal(18,2);default:0" json:"total_price_custom"`
+	TotalProduct    int64     `gorm:"default:0" json:"total_product"`
+	Status          string    `gorm:"size:50;type:enum('not sale', 'sale', 'bundle');default:'not sale'" json:"status"`
+	Barcode         string    `gorm:"size:255;unique;not null" json:"barcode"`
 	CategoryID      *uint64    `json:"category_id"`
 	TagColorID      *uint64    `json:"tag_color_id"`
 	WarehouseType   string    `gorm:"type:enum('type1', 'type2');default:'type1';size:50" json:"warehouse_type"`
