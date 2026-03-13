4f4f8904ca91d482cba925b13f603d600d1fef9c
windisiswandi <windisiswandi89@gmail.com>
2026-03-13 23:40:34 +0800

    feat: add extra product status

--- models/products.go
+++ models/products.go
@@ -33,6 +33,7 @@ type Product struct {
 	StagingStage  		*string     `gorm:"type:enum('process','approve');size:7" json:"staging_stage"` // enum
 	WarehouseType 		string      `gorm:"type:enum('type1', 'type2');default:'type1';size:5" json:"warehouse_type"`// enum
 	IsSo          		*string     `gorm:"type:enum('check','done','lost','addition');size:8" json:"is_so"`
+	IsExtraProduct    	bool     	`gorm:"default:false" json:"is_extra_product"`
 	UserSo        		*uint64     `json:"user_so"`
 	
 	CreatedAt     		time.Time   `json:"created_at"`
